a9e23c6cb514b36398da31965596d02d98e71699
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-02-14 23:33:15 +0530

    change in variable a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a=10;
+        int a=56;
         int b=20;
         int z= a+b;
         System.out.println(z);
